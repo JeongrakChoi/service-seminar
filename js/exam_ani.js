8ddb3ca0623618ea8d81da7fa45674521019a3f1
최정락 <jeongrak.choi@hancom.com>
2024-04-18 16:12:37 +0900

    upload

---
+++ js/exam_ani.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+  var typed = new Typed('.ani_tit', {
+    strings: ['신나는 타자연습!', '잠시만 기다려주세요.'],
+    typeSpeed: 50,
+    loop: true,
+    backDelay: 2000
+  });
+})
+
--- js/ui.js
+++ js/ui.js
@@ -1,29 +1,21 @@
 $(function(){
   //풀페이지
   var myFullpage = new fullpage('#fullpage', {
-    afterLoad: function(){
-      shuffle(); // 첫 번째 로딩 시에는 실행
+    afterLoad: function(origin, destination){
+      var sectionId = destination.item.getAttribute('id'); // 현재 섹션의 ID 가져오기
+      shuffle(sectionId); // 해당 섹션의 타이틀만 셔플
     },
-    onLeave: function(){
-      resetShuffle()
+    onLeave: function(origin, destination, direction){
+      resetShuffle(); 
     }
   });
 
-
-  function shuffle(){
-    var isVisible1 = false;
-    var tit1 = $(".page_tit").text();
-
-    if (!isVisible1){
-      $('.active .page_tit span').shuffleText(tit1);
-      isVisible1=true;
-    }
+  function shuffle(sectionId){
+    var tit = $("#" + sectionId + " .page_tit").text(); // 해당 섹션의 타이틀 가져오기
+    $('#' + sectionId + ' .page_tit span').shuffleText(tit);
   }
 
-  function resetShuffle(){
+  function resetShuffle(sectionId){
     $(".page_tit span").html('');
   }
-
-
 })
-
